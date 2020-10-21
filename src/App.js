09e55f34d5cf397a5f23a9a67114a90baf10ffbf
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-10-21 18:50:05 -0500

    added containers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from "react";
-import jumboData from "./fixtures/jumbo";
-import Jumbotron from "./components/jumbotron";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => {
-        console.log(item);
-        return (
-          <Jumbotron key={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt} />
-            </Jumbotron.Pane>
-          </Jumbotron>
-        );
-      })}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
