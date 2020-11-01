c5e1bb9d89c7e2805af780ea41a0ada035013e3e
Daniel <danielkamara@Daniels-MacBook-Pro-2.local>
2020-11-01 15:38:35 -0600

    finished the netflix header section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Feature, OptForm } from "../components";
 import { HeaderContainer } from "../containers/header";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
@@ -8,10 +9,26 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Feature>
+          <Feature.Title>Unlimited films, TV, programs and more</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at anytime.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
+
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
     </>
   );
 }
