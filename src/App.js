fa8dd15ff3a7fb8c270446ad8a7340af1f6640fb
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-11-17 21:30:29 -0600

    changed routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = { name: "daniel" };
+
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-
-      <Route exact path="/signin">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin />
-      </Route>
+      </IsUserRedirect>
 
-      <Route exact path="/signup">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup />
-      </Route>
-
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
