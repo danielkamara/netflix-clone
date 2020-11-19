01ac85a5654253496b2f19400dc82ac34db36da8
Daniel <danielkamara@Daniels-MacBook-Pro-2.local>
2020-11-19 00:46:22 -0600

    created hooks for browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 
 export default function Browse() {
+
+
+    // need the series and the films
+    // need slides
+    // pass slides to the browse container
 return <p>Hello from the browse page!!!</p>
 }
