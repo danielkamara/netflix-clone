ffff0c3df2aa67bd2ce60e84f5167bb887f2f263
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-11-19 18:42:06 -0600

    started on the profiles component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,6 @@
-import React from 'react'
-
+import React from "react";
+import { SelectProfileContainer } from "./profiles";
 
 export function BrowseContainer({ slides }) {
-    return <p>Hello</p>
+  return <SelectProfileContainer />;
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,14 +1,25 @@
-import React from 'react' 
-import * as ROUTES from '../constants/routes'
-import logo from '../logo.svg'
-import { Header } from '../components'
+import React from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
-
-export function SelectProfileContainer({ user, setPeofile}) {
-    return 
+export function SelectProfileContainer({ user, setProfile }) {
+  return (
     <>
-    <Header bg={false}>
-        
-    </Header>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+        </Header.Frame>
+      </Header>
+      <Profiles>
+        <Profile.Title>Who's Watching?</Profile.Title>
+        <Profiles.List>
+          <Profiles.User>
+            <Profile.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
     </>
+  );
 }
