3ba1b529b16618d5489c5c2ef38823d4cf022210
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-12-15 13:38:51 -0600

    saving changes

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, createContext } from "react";
+import React, { useState, useContext, createContext } from "react";
 
 import {
   Container,
@@ -55,6 +55,36 @@ Card.Entities = function CardEntities({ children, ...restProps }) {
 
 Card.Meta = function CardMeta({ children, ...restProps }) {
   return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Feature = function CardFeature({ children, category, ...restProps }) {
+  const { showFeature, itemFeature, setShowFeature } = useContext(
+    FeatureContext
+  );
+
+  return showFeature ? (
+    <Feature
+      {...restProps}
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+    >
+      <Content>
+        <FeatureTitle>{itemFeature.Title}</FeatureTitle>
+        <FeatureText>{itemFeature.Title}</FeatureText>
+        <FeatureClose onClick={() => setShowFeature(false)}>
+          <img src="/images/icons/close.png" alt="Close" />
+        </FeatureClose>
+      </Content>
+      <Group margin="30px 0" flexDirection="row" alignItems="center">
+        <Maturity rating={itemFeature.maturity}>
+          {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+        </Maturity>
+        <FeatureText fontWeight="bold">
+          {itemFeature.genre.charAt(0).toUpperCase() +
+            itemFeature.genre.slice(1)}
+        </FeatureText>
+      </Group>
+    </Feature>
+  ) : null;
 };
 
 Card.Item = function CardItem({ item, children, ...restProps }) {
