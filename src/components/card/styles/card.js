2c560f0eeb325510e08c9ac0b975e5d39ac46fe8
Daniel <danielkamara@Daniels-MacBook-Pro-2.local>
2020-12-16 12:03:38 -0600

    styling cards section' git push git add .

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -1,7 +1,12 @@
 import styled from "styled-components/macro";
 
 export const Title = styled.p`
-
+font-size: 24px;
+color: #e5e5e5;
+font-weight: bold;
+margin-left: 56px;
+margin-right: 56px;
+margin-top: 0;
 `;
 
 export const Container = styled.div`
@@ -9,9 +14,30 @@ export const Container = styled.div`
   flex-direction: column;
   margin-bottom: 50px;
   box-sizing; border-box;
+  >${Title} {
+    @media (max-width: 1000px) {
+      margin-left: 30px;
+    }
+  }
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
 `;
 
-export const Group = styled.div``;
+export const Group = styled.div`
+display: flex;
+flex-direction: ${({ flexDirection }) =>
+flexDirection === 'row' ? 'row' : 'column'};
+${({ alignItems }) => alignItems && `align-items: ${alignItems}`}
+${({ margin }) => margin && `margin: ${margin}`}
+
+> ${Container}:first-of-type {
+  @media (min-width: 1100px) {
+    margin-top: -150px;
+  }
+}
+`;
 
 export const SubTitle = styled.p``;
 
@@ -29,9 +55,19 @@ export const Maturity = styled.div``;
 
 export const Content = styled.div``;
 
-export const Meta = styled.div``;
+export const Meta = styled.div`
+display: none;
+position: absolute;
+bottom: 0;
+padding: 10px;
 
-export const Entities = styled.div``;
+`;
+
+export const Entities = styled.div`
+display: flex;
+flex-direction: row;
+
+`;
 
 export const Item = styled.div``;
 
